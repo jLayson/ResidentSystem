a8b031eff3f233f9655e00f74e4409a44215f226
Justin <Justin>
2017-05-28 21:35:40 +0800

    Restored overwritten changes

--- routes/web.php
+++ routes/web.php
@@ -63,7 +63,7 @@ Route::get('/listreports', 'ReportController@adminListReports');
 //Admin /guest actions
 Route::get('/guestform', 'GuestController@addGuestForm');
 Route::post('/addguest', 'GuestController@addGuestRecord');
-Route::get('/listguests', 'GuestController@listGuestRecord');
+Route::get('/listguests/{offset}', 'GuestController@listGuestRecord');
 Route::get('/guestdeparture/{id}', 'GuestController@updateGuestDeparture');
 
 
@@ -76,5 +76,6 @@ Route::get('/export/report', 'PdfExportController@reportExport');
 //PDF Export /Visitor Notification
 Route::get('/export/visitor/notification', 'PdfExportController@visitorNotifExport');
 
-//PDF Export /Guest
-Route::get('/export/guest', 'PdfExportController@guestExport');
+//AJAX Test
+Route::get('/ajaxtest/{offset}', 'VisitorNotificationController@visitorGetAjaxForm');
+Route::post('/ajaxgetvisitors', 'VisitorNotificationController@visitorGetAjax');
